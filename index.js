149244ea2d629d0c340d99e60d496dfed405ccbe
Thangtheregista <thangthergista@gmail.com>
2024-03-23 23:31:26 +0700

    third commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Product = require("./models/product.model.js");
 
 /*middleware config*/
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("hello from node hallo");
@@ -49,6 +50,21 @@ app.put("/api/product/:id", async (req, res) => {
 
     const updatedProduct = await Product.findById(id);
     res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//delete a product
+
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
